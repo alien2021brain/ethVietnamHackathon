e0c8ce9f2b945d93bdae764d559e99188f12faa4
Dzung Nguyen <Dzung@infodrive-solutions.com>
2022-11-10 20:23:08 +0700

    Init

---
+++ SocialRecoveryDAO/Frontend/src/components/Admin/AdUser/AllUsersList.js
@@ -0,0 +1,216 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, withRouter } from "react-router-dom";
+import FooterAdmin from "../FooterAdmin";
+import TopBarAdmin from "../TopBarAdmin";
+import SideBarAdmin from "../SideBarAdmin";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import CustomTable from "../../CustomTable";
+
+function AllUsersList(props) {
+  const UserId = localStorage.getItem("id");
+  const columns = [
+    {
+      title: "Name",
+      field: "Name",
+      render: (rowData) => (
+        <Link
+          className="mr-2 btn btn-outline-primary wid"
+          to={`/candidatesInfo/${rowData.UserPublicKey}`}
+        >
+          {rowData.Name}{" "}
+        </Link>
+      ),
+    },
+    { title: "Email", field: "Email" },
+    { title: "Mobile Number", field: "MobileNumber" },
+    { title: "Total Deposit", field: "TotalDeposits" },
+    // { title: "Resume", field: 'Resume', render: rowData => <a href={rowData.Resume} target="_blank"><FileOpenIcon src={rowData.Resume} /></a> },
+  ];
+  const tableRef = React.createRef();
+  //Get compnay name
+  const [notes, getNotes] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [dltnotes, getdltNotes] = useState();
+  const [myId, setId] = useState(12);
+  const url = "http://localhost:3001/getUser";
+  useEffect(() => {
+    getAllNotes(myId);
+  }, [myId]);
+
+  const getAllNotes = (id) => {
+    axios
+      .post(url, { UserId: id })
+      .then((response) => {
+        const allNotes = response.data.data;
+
+        getNotes(allNotes);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  };
+
+  //Update Records
+  const handleRowUpdate = (newData, oldData, resolve, reject) => {
+    if (newData != "") {
+      axios.post("http://localhost:3001/updateUser", newData).then((res) => {
+        toast.success("User Update Successfully", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          getAllNotes(myId);
+          props.history.push("/allUsersList");
+        }, 2000);
+      });
+    } else {
+      toast.error("Invalid Input", {
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  return (
+    <>
+      {/*<div className="container adm"> */}
+      <div className="adm">
+        <div className="mb-3">
+          <div className="row" id="main">
+            <div id="page-top">
+              <div id="wrapper">
+                {/*sidebar component*/}
+                <SideBarAdmin />
+                {/*End sidebar component*/}
+
+                {/*  <!-- Content Wrapper --> */}
+                <div id="content-wrapper" className="d-flex flex-column">
+                  <div id="content">
+                    {/* <!-- Topbar component--> */}
+                    <TopBarAdmin />
+                    {/*  <!-- End of Topbar --> */}
+
+                    <div className="container-fluid">
+                      {/*  <!-- Page Heading --> */}
+                      <div className="d-sm-flex align-items-center justify-content-between mb-[10px]">
+                        <h1 className="tw-text-[32px] tw-text-black tw-font-bold">
+                          User List
+                        </h1>
+                      </div>
+
+                      <div className="tw-w-full tw-mt-[35px]">
+                        <div className="">
+                          {/* <!-- Content Row --> */}
+                          {(() => {
+                            if (
+                              UserId ==
+                              "0xF2379d114d065c54d9C49dB8bB3a4035550c4e71"
+                            ) {
+                              return (
+                                <div>
+                                  <CustomTable
+                                    title="User List"
+                                    columns={columns}
+                                    data={notes}
+                                    onSelectionChange={(rows) =>
+                                      setSelectedRows(rows)
+                                    }
+                                    editable={{
+                                      onRowUpdate: (newData, oldData) =>
+                                        new Promise((resolve, reject) => {
+                                          handleRowUpdate(
+                                            newData,
+                                            oldData,
+                                            resolve
+                                          );
+                                        }),
+                                      // onRowDelete: (selectedRow) =>
+                                      //     new Promise((resolve) => {
+                                      //     handleRowDelete(selectedRow, resolve)
+                                      // }),
+                                    }}
+                                    options={{
+                                      actionsColumnIndex: -1,
+                                      addRowPosition: "first",
+                                      selection: true,
+
+                                      headerStyle: {
+                                        backgroundColor: "#eaeffb",
+                                        color: "#000",
+                                      },
+                                      loadingType: "none",
+                                    }}
+                                    // actions={[
+                                    //     {
+                                    //         icon: 'delete',
+                                    //         tooltip: "Delete all selected rows",
+                                    //         onClick: () => handleBulkDelete()
+
+                                    //     }, {
+                                    //         icon: Refresh,
+                                    //         tooltip: 'Refresh Data',
+                                    //         isFreeAction: true,
+                                    //         onClick: () => tableRef.current && tableRef.current.onQueryChange()
+                                    //     }
+                                    // ]}
+                                  />
+                                </div>
+                              );
+                            } else {
+                              return (
+                                <div>
+                                  <CustomTable
+                                    title="All Candidates List"
+                                    columns={columns}
+                                    data={notes}
+                                    onSelectionChange={(rows) =>
+                                      setSelectedRows(rows)
+                                    }
+                                    options={{
+                                      actionsColumnIndex: -1,
+                                      addRowPosition: "first",
+                                      selection: true,
+
+                                      headerStyle: {
+                                        backgroundColor: "#eaeffb",
+                                        color: "#000",
+                                      },
+                                      loadingType: "none",
+                                    }}
+                                  />
+                                </div>
+                              );
+                            }
+                          })()}
+                          {/* Content Row End*/}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* <!-- End of Content Wrapper --> */}
+              </div>
+
+              {/* <!-- Footer --> */}
+              <FooterAdmin />
+              {/*<!-- End of Footer --> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+}
+export default withRouter(AllUsersList);
---
+++ SocialRecoveryDAO/Frontend/src/components/Admin/Enroll.js
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, withRouter } from "react-router-dom";
+import FooterAdmin from "./FooterAdmin";
+import TopBarAdmin from "./TopBarAdmin";
+import SideBarAdmin from "./SideBarAdmin";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import CustomTable from "../CustomTable";
+
+const Enroll = () => {
+  const columns = [
+    { title: "SP Address", field: "SPAddress" },
+    { title: "Rate", field: "Rate" },
+    { title: "Rating", field: "Rating" },
+  ];
+
+  const [notes, getNotes] = useState([]);
+
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [dltnotes, getdltNotes] = useState();
+  const [myId, setId] = useState(12);
+  const url = "http://localhost:3001/getCompanyList";
+  useEffect(() => {
+    getAllNotes(myId);
+  }, [myId]);
+  const getAllNotes = (id) => {
+    axios
+      .post(url, { userId: id })
+      .then((response) => {
+        const allNotes = response.data.payload.payload;
+        getNotes(allNotes);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  };
+
+  //Update Records
+  const handleRowUpdate = (newData, oldData, resolve, reject) => {
+    if (newData != "") {
+      axios
+        .post("http://localhost:3001/updateCompanyById", newData)
+        .then((res) => {
+          toast.success(res.data.message, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          setTimeout(() => {
+            getAllNotes(myId);
+          }, 2000);
+        })
+        .catch((e) => console.log("Error", e));
+    } else {
+      toast.error("Invalid Input", {
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  // Delete Contac list
+  const urldlt = "http://localhost:3001/deleteCompanyById/";
+  const handleRowDelete = (selectedRow, resolve) => {
+    const upId = selectedRow.CompanyId;
+    axios
+      .post(urldlt, { id: upId })
+      .then((response) => {
+        toast.success("Record Deleted successfully!", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          getAllNotes(myId);
+        }, 2000);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  };
+  //Delete Multipal row
+  const handleBulkDelete = () => {
+    const urldt = "http://localhost:3001/deleteAllCompanysById/";
+    const updatedData = notes.filter((row) => selectedRows.includes(row));
+    console.log("dat____", updatedData);
+    const updt = updatedData.map((u) => u.CompanyId);
+    axios
+      .post(urldt, { ids: updt })
+      .then((response) => {
+        console.log("delete_____", response);
+        toast.success("Record Deleted successfully!", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          getAllNotes(myId);
+        }, 2000);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+    getdltNotes(updatedData);
+  };
+
+  return (
+    <>
+      <div className="adm">
+        <div className="mb-3">
+          <div className="row" id="main">
+            <div id="page-top">
+              <div id="wrapper">
+                <SideBarAdmin />
+
+                <div id="content-wrapper" className="d-flex flex-column">
+                  <div id="content">
+                    <TopBarAdmin />
+
+                    <div className="container-fluid">
+                      <div className="mb-4 d-sm-flex align-items-center justify-content-between">
+                        <div
+                          className="font-bold text-black"
+                          style={{
+                            fontSize: "18px",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Enroll Me
+                        </div>
+                      </div>
+
+                      <div className="compny">
+                        <div className="col-sm-12 rsp">
+                          <CustomTable
+                            title=""
+                            columns={columns}
+                            data={notes}
+                            onSelectionChange={(rows) => setSelectedRows(rows)}
+                            options={{
+                              actionsColumnIndex: -1,
+                              addRowPosition: "first",
+                              selection: true,
+
+                              headerStyle: {
+                                backgroundColor: "#eaeffb",
+                                color: "#000",
+                              },
+                              loadingType: "none",
+                            }}
+                            actions={[
+                              {
+                                icon: "delete",
+                                tooltip: "Delete all selected rows",
+                                onClick: () => handleBulkDelete(),
+                              },
+                            ]}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <FooterAdmin />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <ToastContainer />
+    </>
+  );
+};
+export default withRouter(Enroll);
